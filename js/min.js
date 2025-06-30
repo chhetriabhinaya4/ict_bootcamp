7c8f2002c71fd567f6c4335e645b0224fc5f60e7
Abhinaya Chhetri <chhetriabhinaya4@gmail.com>
2025-06-30 16:47:03 +0545

    rechecked al

---
+++ js/min.js
@@ -0,0 +1,10 @@
+function responsive(){
+    let x= document.getElementById("navbar");
+
+    if(x.className==="navbar"){
+        x.className+= " responsive" ;
+    }
+    else{
+        x.className="navbar";
+    }
+}
